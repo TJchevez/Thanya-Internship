e8f11b82225791e8899d08494d90dd4c4ae22a9b
Thanya <cheveztj@gmail.com>
2025-01-28 13:17:19 -0500

    dynamic-carousel-properties

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios"
+import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 const HotCollections = () => {
   const [carousel, setCarousel] = useState([])
   async function datacollection() {
     const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections")
    setCarousel(data)
-   console.log(data)
     }
   useEffect(() => {
     datacollection();
@@ -23,25 +20,25 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(6).fill(0).map((_, index) => (
+          {carousel.map((nft, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={nft.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={nft.authorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{nft.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{nft.code}</span>
                 </div>
               </div>
             </div>
