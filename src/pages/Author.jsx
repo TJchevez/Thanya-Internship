fda346f848df424664827103dd3a7008475ca355
Thanya <cheveztj@gmail.com>
2025-03-20 13:21:46 -0400

    dynamic routing and data applied-skeleton loading state is left to apply

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -2,112 +2,94 @@ import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
 
 
-
 const Author = () => {
-  const { authorId } = useParams();
-  const [posts, setPosts] = useState([]);
+  const { id } = useParams();
+  const [authorData, setAuthorData] = useState(null);
   const [items, setItems] = useState([]);
-  
 
   useEffect(() => {
-    async function fetchPosts() {
-      const { data } = await axios.get(
-        `https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems?author=${authorId}`
-      );
-      console.log(data);
-      setPosts(data);
-      setItems(data);
-    }
-    fetchPosts();
-  }, [authorId]);
+    async function getAuthorData() {
+      try {
+        const { data } = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`
+        );
 
-  const NewItemComponent = ({ item }) => {
-    return (
-        <div>
-            <h2>{item.title}</h2>
-        </div>
-        
-    );
-  };
+        setAuthorData(data);
+        setItems(data.nftCollection || []); 
+      } catch (error) {
+        console.error("Error fetching author data:", error);
+      }
+    }
+
+    getAuthorData();
+  }, [id]);
+
+  if (!authorData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
+
         <section
           id="profile_banner"
           aria-label="section"
           className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
           style={{ background: `url(${AuthorBanner}) top` }}
         ></section>
-        <>
-        {items.length > 0 ? (
-                        items.map(item => (
-                            <NewItemComponent key={item.id} item={item} />
-                        ))
-                    ) : (
-                        <p>No items found!</p>
-                    )}
-          {posts.length > 0 && (
-            <div key={0}>
-              <section aria-label="section">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="d_profile de-flex">
-                        <div className="de-flex-col">
-                          <div className="profile_avatar">
-                            <img src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                            <div className="profile_name">
-                            <h4>
-                                Monica Lucas
-                                <span className="profile_username">
-                                  @monicaaaa
-                                </span>
-                                <span id="wallet" className="profile_wallet">
-                                  UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                                </span>
-                                <button id="btn_copy" title="Copy Text">
-                                  Copy
-                                </button>
-                              </h4>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="profile_follow de-flex">
-                          <div className="de-flex-col">
-                            <div className="profile_follower">
-                              573 followers
-                            </div>
-                            <Link to="#" className="btn-main">
-                              Follow
-                            </Link>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-md-12">
-                      <div className="de_tab tab_simple">
-                        <AuthorItems />
+
+        <section aria-label="section">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="d_profile de-flex">
+                  <div className="de-flex-col">
+                    <div className="profile_avatar">
+                      <img src={authorData.authorImage} alt="Author" />
+                      <i className="fa fa-check"></i>
+                      <div className="profile_name">
+                        <h4>
+                          {authorData.authorName}
+                          <span className="profile_username">@{authorData.tag}</span>
+                          <span id="wallet" className="profile_wallet">
+                            {authorData.address}
+                          </span>
+                          <button id="btn_copy" title="Copy Text">
+                            Copy
+                          </button>
+                        </h4>
                       </div>
                     </div>
                   </div>
+                  <div className="profile_follow de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_follower">
+                        {authorData.followers} followers
+                      </div>
+                      <Link to="#" className="btn-main">
+                        Follow
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-              </section>
+              </div>
+
+              <div className="col-md-12">
+                <div className="de_tab tab_simple">
+                  <AuthorItems items={items} />
+                </div>
+              </div>
             </div>
-          )};
-    </>
+          </div>
+        </section>
       </div>
     </div>
   );
 };
 
-
 export default Author;
-
-
--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,72 +1,81 @@
 import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
-import axios from "axios"
+import axios from "axios";
+
+
 
 const ItemDetails = () => {
-  const { nftId } = useParams();
-  const [posts, setPosts] = useState([]);
+  const { id } = useParams();
+  const [nftItem, setNftItem] = useState(null);
+
   useEffect(() => {
-    async function fetchPosts() {
-      const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems?itemDetails=${nftId}`);
-      setPosts(data);
+    async function fetchNftItem() {
+      try {
+        const { data } = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${id}`
+        );
+        setNftItem(data);
+      } catch (error) {
+        console.error("Error fetching NFT details:", error);
+      }
     }
-    fetchPosts();
-  }, [])
+    fetchNftItem();
+  }, [id]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  if (!nftItem) {
+    return <div>Loading...</div>; 
+  }
+
   return (
-    <>
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
-        {posts.length > 0 && (
-            <div key={0}>
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
               <div className="col-md-6 text-center">
                 <img
-                  src={nftImage}
+                  src={nftItem.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
                   alt=""
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>{nftItem.title + " #" + nftItem.tag}</h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
                       <i className="fa fa-eye"></i>
-                      100
+                      {nftItem.views}
                     </div>
                     <div className="item_info_like">
                       <i className="fa fa-heart"></i>
-                      74
+                      {nftItem.likes}
                     </div>
                   </div>
                   <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
+                  {nftItem.description}
                   </p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${nftItem.ownerId}`}>
+                            <img className="lazy" src={nftItem.ownerImage} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${nftItem.ownerId}`}>
+                          {nftItem.ownerName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -77,13 +86,15 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${nftItem.creatorId}`}>
+                            <img className="lazy" src={nftItem.creatorImage} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${nftItem.creatorId}`}>
+                          {nftItem.creatorName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -91,7 +102,7 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>{nftItem.price}</span>
                     </div>
                   </div>
                 </div>
@@ -99,11 +110,8 @@ const ItemDetails = () => {
             </div>
           </div>
         </section>
-       </div>
-      )}
       </div>
     </div>
-    </>
   );
 };
 
