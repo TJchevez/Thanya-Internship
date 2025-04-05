549d2574e10464f31f244bba8a3fba05212560d3
Thanya <cheveztj@gmail.com>
2025-04-05 17:02:02 -0400

    From main branch

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -8,6 +8,7 @@ import AOS from "aos";
 import 'aos/dist/aos.css';
 
 const SkeletonLoader = () => (
+  // From Main!!! DO NOT REMOVE
   <>
     <div className="nft_coll">
       <div className="nft_wrap">
